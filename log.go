7ec584979ef4e8bb9415b3b307eb5f243f12d080
snowmerak <snowmerak@outlook.com>
2022-02-06 15:44:05 +0900

    make log struct

---
+++ log.go
@@ -0,0 +1,8 @@
+package main
+
+type Log struct {
+	UnixTime int64  `parquet:"name=unix_time, type=INT64" json:"unix_time"`
+	AppID    int32  `parquet:"name=app_id, type=INT32" json:"app_id"`
+	Level    int32  `parquet:"name=level, type=INT32" json:"level"`
+	Message  string `parquet:"name=message, type=BYTE_ARRAY, convertedtype=UTF8, encoding=PLAIN_DICTIONARY" json:"message"`
+}
