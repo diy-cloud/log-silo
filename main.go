4bb5b6eff49a2d6d03174c95438494156d618341
snowmerak <snowmerak@outlook.com>
2022-02-06 20:55:44 +0900

    add PATH env

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"path/filepath"
+	"runtime"
 	"time"
 
 	"github.com/snowmerak/log-silo/log"
@@ -11,17 +12,22 @@ import (
 )
 
 func main() {
-	if err := os.MkdirAll(filepath.Join(".", "logs"), 0777); err != nil {
+	path := os.Getenv("PATH")
+	if path == "" {
+		path = filepath.Join(".", "logs")
+	}
+
+	if err := os.MkdirAll(path, 0777); err != nil {
 		panic(err)
 	}
 
-	f, err := local.NewLocalFileWriter(filepath.Join(".", "log", time.Now().Format(time.RFC3339)+".parquet"))
+	f, err := local.NewLocalFileWriter(filepath.Join(path, time.Now().Format(time.RFC3339)+".parquet"))
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
 
-	pw, err := writer.NewParquetWriter(f, new(log.Log), 8)
+	pw, err := writer.NewParquetWriter(f, new(log.Log), int64(runtime.NumCPU()))
 	if err != nil {
 		panic(err)
 	}
