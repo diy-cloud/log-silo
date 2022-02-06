ba11e07269a1ffacda5531ef4b22ba2b502259d1
snowmerak <snowmerak@outlook.com>
2022-02-06 18:59:34 +0900

    add kafka

--- receiver.go
+++ receiver.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/nats-io/nats.go"
+	"github.com/segmentio/kafka-go"
 	"github.com/snowmerak/log-silo/util/signal"
 	"github.com/xitongsys/parquet-go/writer"
 )
@@ -16,7 +20,9 @@ func RunReceiver(pw *writer.ParquetWriter) {
 	case "nats":
 		RunNats(pw)
 	case "kafka":
+		RunKafka(pw)
 	case "http":
+		RunHTTP(pw)
 	}
 }
 
@@ -52,4 +58,59 @@ func RunNats(pw *writer.ParquetWriter) {
 	defer sub.Unsubscribe()
 
 	<-signal.NewTerminate()
+}
+
+func RunKafka(pw *writer.ParquetWriter) {
+	topic := os.Getenv("KAFKA_TOPIC")
+	if topic == "" {
+		topic = "log"
+	}
+	partition := 0
+	if value := os.Getenv("KAFKA_PARTITION"); value != "" {
+		v, err := strconv.Atoi(value)
+		if err == nil {
+			partition = v
+		}
+	}
+	network := "tcp"
+	if value := os.Getenv("KAFKA_NETWORK"); value != "" {
+		network = value
+	}
+	url := "localhost:9092"
+	if value := os.Getenv("KAFKA_URL"); value != "" {
+		url = value
+	}
+
+	conn, err := kafka.DialLeader(context.Background(), network, url, topic, partition)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	buf := make([]byte, 8*1024)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			panic(err)
+		}
+		l := new(Log)
+		if err := json.Unmarshal(buf[:n], l); err != nil {
+			l.AppID = -1
+			l.Level = ERROR
+			l.Message = err.Error()
+			l.UnixTime = time.Now().Unix()
+		}
+		if err := pw.Write(l); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func RunHTTP(pw *writer.ParquetWriter) {
+	http.HandleFunc("/push", func(rw http.ResponseWriter, r *http.Request) {
+
+	})
+	http.HandleFunc("/pull", func(rw http.ResponseWriter, r *http.Request) {
+
+	})
 }
