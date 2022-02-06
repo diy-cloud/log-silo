776ea98fc531e1c0818eb9b590a866376ebd28ed
snowmerak <snowmerak@outlook.com>
2022-02-06 15:43:36 +0900

    create signal package

---
+++ util/signal/signal.go
@@ -0,0 +1,32 @@
+package signal
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
+
+const signalNum = 2
+
+var sigs = make(chan os.Signal, 1)
+var done = make(chan struct{}, signalNum)
+var once = new(sync.Once)
+
+func NewTerminate() <-chan struct{} {
+	once.Do(func() {
+		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+		go func() {
+			sig := <-sigs
+			fmt.Println()
+			fmt.Println(sig)
+			for i := 0; i < signalNum; i++ {
+				done <- struct{}{}
+			}
+		}()
+	})
+
+	return done
+}
